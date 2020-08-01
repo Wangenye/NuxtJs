8db75b535cbe0e46648f60e70a63538d7e03c916
Neal <darc@localhost.localdomain>
2020-08-01 15:30:42 +0300

    Nuxt Lesson App

---
+++ components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt-link to="/" class="brand">
+      Real World Events
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/"> List </nuxt-link>
+
+      <nuxt-link to="/">
+        Create Events
+      </nuxt-link>
+    </nav>
+  </div>
+</template>
